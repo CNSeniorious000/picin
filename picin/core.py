dba889936e39760080ae2e6f621507b6309772a4
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-07-17 22:25:33 +0800

    use yaml to save cached averages

--- picin/core.py
+++ picin/core.py
@@ -5,7 +5,7 @@ import cv2
 
 
 class Image:
-    images = {}
+    averages = {}
 
     @cache
     def __new__(cls, *args, **kwargs):
@@ -24,26 +24,17 @@ class Image:
         show()
         return self
 
-    @staticmethod
-    def get_square(self: "Image"):
-        try:
-            return self.images[self]
-        except KeyError:
-            h, w = self.buffer.shape[:2]
-            if h > w:
-                y = (h - w) // 2
-                result = self.buffer[y:y + w]
-            elif w > h:
-                x = (w - h) // 2
-                result = self.buffer[:, x:x + h]
-            else:
-                result = self.buffer
-            self.images[self] = result
-            return result
-
     @cached_property
     def square(self) -> np.ndarray:
-        return self.get_square(self)
+        h, w = self.buffer.shape[:2]
+        if h > w:
+            y = (h - w) // 2
+            return self.buffer[y:y + w]
+        elif w > h:
+            x = (w - h) // 2
+            return self.buffer[:, x:x + h]
+        else:
+            return self.buffer
 
     @cache
     def resized(self, length):
@@ -51,7 +42,12 @@ class Image:
 
     @cached_property
     def average(self) -> np.ndarray:
-        return self.square.mean((0, 1))
+        try:
+            return self.averages[self.path]
+        except KeyError:
+            result: np.ndarray = self.square.mean((0, 1))
+            self.averages[self.path] = result.tolist()
+            return result
 
     def distance(self, color):
         return sum((self.average - color) ** 2) ** 0.5
@@ -66,23 +62,21 @@ class Image:
         return f"<Image from {self.path!r}>"
 
     @classmethod
-    def save_cache(cls, path="cache.pkl", slim=True):
-        if slim:
-            for image in cls.images:
-                if hasattr(image, "buffer"):
-                    del image.buffer  # original image
-
-        from pickle import dumps
-        from blosc2 import compress
-        with open(path, "wb") as f:
-            f.write(compress(dumps(cls.images), 1, cname="lz4hc"))
+    def save_cache(cls, path="averages.yaml"):
+        from yaml import dump
+        with open(path, "w") as f:
+            f.write(dump(cls.averages))
 
     @classmethod
-    def load_cache(cls, path="cache.pkl"):
-        from pickle import loads
-        from blosc2 import decompress
-        with open(path, "rb") as f:
-            cls.images = loads(decompress(f.read()))
+    def load_cache(cls, path="averages.yaml"):
+        from yaml import load, CLoader
+        with open(path) as f:
+            averages = load(f.read(), CLoader)
+
+        for key, val in averages.items():
+            averages[key] = np.array(val)
+
+        cls.averages.update(averages)
 
 
 if __name__ == '__main__':
@@ -90,18 +84,17 @@ if __name__ == '__main__':
 
     register_heif_opener()
     for i in range(10):
-        print(len(Image.images), end=" ")
         _ = Image("../input/IMG_20220625_194125.HEIC").average
     print(Image.__new__.cache_info())
 
+    print(Image.averages)
     Image.save_cache()
+    Image.load_cache()
+    print(Image.averages)
 
     """
     buffer是一个property保证了图片的懒加载
-    get_square的缓存保证了相同图片不会重新resize
     __new__的缓存保证了相同图片不会重复加载, 也不会重新计算均值
-    
-    最后, 持久化缓存保证不带有原图
     
     实测大大加快了尤其是HEIF文件的加载速度
     
